219440bf83ff9d4ff73f5dc5e917183a7166d2ea
arjitMathur <mathurarjit04@gmail.com>
2026-01-20 20:28:50 +0530

    Download pickle files at runtime

--- app.py
+++ app.py
@@ -2,24 +2,50 @@ import streamlit as st
 import pickle
 import pandas as pd
 import os
-
-def recommend(movie):
-    movie_index=movies[movies['title']== movie].index[0]
-    distances=similarity[movie_index]
-    movie_index=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
-    l=[]
-    for i in movie_index:
-        l.append(movies.iloc[i[0]].title)
-    return l
+import requests
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-movies = pickle.load(open(os.path.join(BASE_DIR, 'movies.pkl'), 'rb'))
-similarity = pickle.load(open(os.path.join(BASE_DIR, 'similarity.pkl'), 'rb'))
+MOVIES_PATH = os.path.join(BASE_DIR, "movies.pkl")
+SIMILARITY_PATH = os.path.join(BASE_DIR, "similarity.pkl")
+
+MOVIES_URL = "https://github.com/arjitMathur/Movie_recommendation_system/releases/download/models-v1/movies.pkl"
+SIMILARITY_URL = "https://github.com/arjitMathur/Movie_recommendation_system/releases/download/models-v1/similarity.pkl"
 
 
-movies_list=movies['title'].values
-st.title('Movie Recommender System')
+def download_file(url, path):
+    if not os.path.exists(path):
+        with st.spinner(f"Downloading {os.path.basename(path)}..."):
+            response = requests.get(url, timeout=120)
+            response.raise_for_status()
+            with open(path, "wb") as f:
+                f.write(response.content)
+
+
+download_file(MOVIES_URL, MOVIES_PATH)
+download_file(SIMILARITY_URL, SIMILARITY_PATH)
+
+
+movies = pickle.load(open(MOVIES_PATH, "rb"))
+similarity = pickle.load(open(SIMILARITY_PATH, "rb"))
+
+
+def recommend(movie):
+    movie_index = movies[movies["title"] == movie].index[0]
+    distances = similarity[movie_index]
+
+    movie_indices = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
+
+    return [movies.iloc[i[0]].title for i in movie_indices]
+
+
+st.title("Movie Recommender System")
+
+movies_list = movies["title"].values
 
 option = st.selectbox(
     "Select a movie, and we’ll recommend more movies for you.",
@@ -29,7 +55,7 @@ option = st.selectbox(
 )
 
 if st.button("Recommend"):
-    ls=recommend(option)
-    for i in ls:
-        st.write(i)
-
+    if option:
+        recommendations = recommend(option)
+        for movie in recommendations:
+            st.write(movie)
